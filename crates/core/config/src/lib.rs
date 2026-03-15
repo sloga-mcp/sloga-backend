1e3a13c687aa52591b16627b049c13b7cef91bc0
Tom <iamtomahawkx@gmail.com>
2026-03-15 09:15:54 -0700

    fix: don't use a bitop for OR (#676)
    * fix: uname is missing from crond
    
    Signed-off-by: IAmTomahawkx <iamtomahawkx@gmail.com>
    
    * fix: don't use a bitop
    lol, two of us missed that.
    
    ---------
    
    Signed-off-by: IAmTomahawkx <iamtomahawkx@gmail.com>

--- crates/core/config/src/lib.rs
+++ crates/core/config/src/lib.rs
@@ -455,7 +455,7 @@ pub async fn config() -> Settings {
 
     // auto-detect production nodes
     if config.hosts.api.contains("https")
-        && (config.hosts.api.contains("revolt.chat") | config.hosts.api.contains("stoat.chat"))
+        && (config.hosts.api.contains("revolt.chat") || config.hosts.api.contains("stoat.chat"))
     {
         config.production = true;
     }
